d416e16f366f963e0b3f7e78f052a12851e6315c
juanonsoftware <juanonsoftware@gmail.com>
2021-11-20 15:08:32 +0700

    Adding MachineName into Home page

--- AspNetCoreWebAppK8s/Pages/Index.cshtml.cs
+++ AspNetCoreWebAppK8s/Pages/Index.cshtml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace AspNetCoreWebAppK8s.Pages
 {
@@ -11,7 +7,7 @@ namespace AspNetCoreWebAppK8s.Pages
     {
         public void OnGet()
         {
-
+            ViewData["MachineName"] = Environment.MachineName;
         }
     }
 }
